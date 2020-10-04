7bfea2858b4940183e258c7ec8b06fd43fe0a8f4
Natarajan <37583070+Natarajan-1998@users.noreply.github.com>
2020-10-04 12:40:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
 
+const mockUserData=[
+  {name: 'Mark'},
+  {name: 'Jill'}
+  ]
+app.get('/users' , function(req,res){
+  res.json({
+    success: true,
+    message: 'successfully got users. Nice!',
+    users: mockUserData
+  })
+})
 
-app.listen(8000,function(){
-console.log("server is running")
+app.listen(8000,function(){console.log("server is listening")
 })
