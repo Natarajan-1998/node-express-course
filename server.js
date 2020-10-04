ef7ee62b08fd3ba8eb883c920c4d28c6b6d76df9
A V Natarajan <avn1998@gmail.com>
2020-10-04 19:22:38 +0530

    add login POST route

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
 const mockUserData=[
   {name: 'Mark'},
   {name: 'Jill'}
